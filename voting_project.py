e5afd325484f86471c50db89fc44d581b7aed6a5
Shubhradeep Maity <shubhradeepmaity3@gmail.com>
2024-09-26 00:31:20 +0530

    One Admin

--- voting_project.py
+++ voting_project.py
@@ -61,6 +61,34 @@ Me: I implemented the RESTful API endpoints using Express.js and MongoDB. For ex
 Atomic transactions in MongoDB : =>
 To handle concurrent voting, I relied on atomic transactions in MongoDB. When a user votes, their voting status is updated simultaneously with the vote being cast. MongoDB handles this atomically, ensuring that race conditions are avoided. Additionally, if multiple vote requests are received at the exact same time, only the first request is processed, while the others are rejected after checking the voting status.
 
+
+
+
+#voting Algo
+
+http://locaIhost:3000/candidate/vote/65d1d02ae04275593a03dc041  //Parametrised API calls 
+
+router.post('/vote/:candidateID')
+
+candidate.votes.push({user: userId})
+        candidate.voteCount++;
+        await candidate.save();
+
+        // update the user document
+        user.isVoted = true
+        await user.save();
+
+
+   #schema Reference
+   votes: [
+        {
+            user: {
+                type: mongoose.Schema.Types.ObjectId,
+                ref: 'User',   #ref from user schema with id
+                required: true
+            },]
+
+
 # Features
 User sign up and login with Aadhar Card Number and password
 User can view the list of candidates
@@ -96,3 +124,25 @@ Get Profile
 GET /users/profile: Get user profile information
 Change Password
 PUT /users/profile/password: Change user password
+
+
+
+#Only 1 Admin
+In my e-voting system, I implemented a check within the user creation process to ensure that only one admin can exist at any given time. This is achieved by adding logic in the pre-save hook of the Mongoose schema.
+
+Whenever a user is being saved with the admin role, the system first performs a query to check if an admin already exists in the database. I use the findOne() method to look for any user with the role set to admin. If such a user exists, the system throws an error, preventing the new user from being created as another admin. This ensures that no more than one admin can ever exist.
+
+
+// Pre-save hook to hash password and check for existing admin
+userSchema.pre('save', async function (next) {
+    const person = this;
+
+    // If the user is being created with the 'admin' role, check if an admin already exists
+    if (person.role === 'admin') {
+        const existingAdmin = await mongoose.models.User.findOne({ role: 'admin' });
+        if (existingAdmin) {
+            const error = new Error('An admin already exists. Only one admin is allowed.');
+            return next(error);
+        }
+    }
+}
