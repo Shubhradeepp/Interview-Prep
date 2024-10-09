f7f18a5753ceb07ef92ff0cedf67226b9340f8d5
Shubhradeep Maity <shubhradeepmaity3@gmail.com>
2024-10-09 19:36:40 +0530

    Opp And decorator

--- Python/Understand.py
+++ Python/Understand.py
@@ -680,3 +680,38 @@ def greet(name, greeting="Hello"):
 
 hello()
 greet("chai", greeting="hanji ")
+
+
+
+
+
+
+
+# Python program to split a string and 
+# join it using different delimiter
+
+def split_string(string):
+
+	# Split the string based on space delimiter
+	list_string = string.split(' ')
+	
+	return list_string
+
+def join_string(list_string):
+
+	# Join the string based on '-' delimiter
+	string = '-'.join(list_string)
+	
+	return string
+
+# Driver Function
+if __name__ == '__main__':
+	string = 'Geeks for Geeks'
+	
+	# Splitting a string
+	list_string = split_string(string)
+	print(list_string)  #['Geeks', 'for', 'Geeks']
+
+	# Join list of strings into one
+	new_string = join_string(list_string)
+	print(new_string)  #Geeks-for-Geeks
