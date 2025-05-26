be130040240e64ecf4a9e666214d3bc097b7ad3d
Shubhradeep Maity <shubhradeepmaity3@gmail.com>
2025-05-26 11:30:38 +0530

    Intro added

---
+++ intro.py
@@ -0,0 +1,19 @@
+Hi, My name is Shubhradeep Maity
+
+I recently completed my B.tech in computer science and engineering from Netaji Subhash Engineering College with an overall 8.9 cgpa.
+
+Along with my degree i have learned multiple programming languages like C++, JavaScript also few web technologies like for frontend react and for Backend node js, express js.
+
+Also i have done multiple projects and implemented the technologies in my projects, few of the noticeable are Ai chat bot, E-VOTING SYSTEM, meme stream app etc..
+
+
+Talking about my professional journey 
+I have done a few internships 
+as a SDE intern with bluestock Fintech, as BACKEND engineer with aryabhata Technology, where i gained hands on experience on react, node js , express js,
+
+Currently I'm working as a Full-Stack developer at SwingBell Labs, where i contribute to solving many real life problems, 
+One of the significant ones is building a patient care app, where the patient can book an appointment online
+
+Lastly I'm a curious person who believes in team work
+
+Thankyou that's it from my side
